b6ce5a74d642e36f119a01fa4733967a1621190f
trungle26 <leductrung8520@gmail.com>
2023-09-28 08:27:24 +0700

    lesson 8 and 9

---
+++ 06. Menu/baitapolop/app/src/main/java/com/example/myapplication/btvn1Constraint.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class btvn1Constraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btvn1_constraint);
+    }
+}
---
+++ 06. Menu/baitapolop/app/src/main/java/com/example/myapplication/btvn1Relative.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class btvn1Relative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btvn1_relative);
+    }
+}
